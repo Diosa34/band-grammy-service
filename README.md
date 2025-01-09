3c96544e071feacfda059dfe97211cea9d0303a7
Diosa34 <superalex.osa@yandex.ru>
2025-01-09 11:09:08 +0300

    Band-grammy-service separated into two modules

--- README.md
+++
@@ -1,5 +0,0 @@
-```postgresql
-create schema band_grammy_service;
-
-alter schema band_grammy_service owner to test1;
-```
--- src/main/java/com/musicbands/bandgrammyservice/models/MusicGenre.java
+++
@@ -1,8 +0,0 @@
-package com.musicbands.bandgrammyservice.models;
-
-public enum MusicGenre {
-    ROCK,
-    RAP,
-    POST_ROCK,
-    PUNK_ROCK
-}
